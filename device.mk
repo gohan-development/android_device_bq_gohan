afc5d8d3af9fcf94bbc0e8b29bc3d80c546810a0
Fran Palmero <sweetydesigns@gmail.com>
2020-08-15 00:19:27 +0200

    gohan: Update camera config for O
    Temp - Need fixes

--- device.mk
+++ device.mk
@@ -117,7 +117,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl \
+    camera.device@3.2-impl \
+    libqomx_core \
+    libmmcamera_interface \
+    libmmjpeg_interface \
+    libmm-qcamera \
     libshim_camera \
     libcamera_parameters_shim \
     libcamera_ppeiscore_shim \
