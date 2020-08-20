ad5bc2e4f58634fa892a5e71a07fff8872ca2860
kelsiraed <dumbandroid@gmail.com>
2020-08-20 15:00:04 +0200

    gohan: Change path to new location device/qcom/sepolicy-legacy/sepolicy.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -225,7 +225,7 @@ PROTOBUF_SUPPORTED := true
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-include device/qcom/sepolicy/legacy-sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
 
 # Shims
