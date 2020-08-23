d9c02d763a3bdcc6d4b833b3c6c0c075153f461d
Yifan Hong <elsk@google.com>
2020-08-23 19:58:49 +0200

    gohan: uprev Health HAL to 2.0
    Test: adb logcat -e health
    Test: lshal --debug
    Change-Id: I3855f256d79a46c9626f043aac667937ff6414e1

--- device.mk
+++ device.mk
@@ -201,9 +201,8 @@ PRODUCT_PACKAGES += \
 
 # Healthd
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-impl \
+    android.hardware.health@2.0-service
 
 # IPACM
 PRODUCT_PACKAGES += \
