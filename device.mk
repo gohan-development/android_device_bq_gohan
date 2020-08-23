3143a32e03a6a217394a798a74c4fd67a96dfb4c
Mrinal Ghosh <mg712702@gmail.com>
2020-08-24 00:20:06 +0200

    gohan: Build android.hardware.bluetooth.a2dp

--- device.mk
+++ device.mk
@@ -114,6 +114,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
+    android.hardware.bluetooth.a2dp@1.0-impl \
+    android.hardware.bluetooth.a2dp@1.0-service \
     libbt-vendor
 
 # Camera
