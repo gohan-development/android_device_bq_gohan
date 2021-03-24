9c0d6d07a6dd5909ebd8a87a2d37944a4ee39797
ZeeLog <Danjel.Zammit@gmail.com>
2021-03-24 22:21:08 +0100

    gohan: Shim missing symbols in libcam.vidhance.so

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -226,7 +226,8 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib/libizat_core.so|libshims_get_process_name.so \
     /system/vendor/lib/libmmcamera_ppeiscore.so|libcamera_ppeiscore_shim.so \
     /system/vendor/lib/libflp.so|libshims_flp.so \
-    /system/vendor/lib/hw/camera.msm8952.so|libshim_camera.so
+    /system/vendor/lib/hw/camera.msm8952.so|libshim_camera.so \
+    /system/lib/libcam.vidhance.so|libcamera_ppeiscore_shim.so
 
 # Vendor Interface Manifest
 DEVICE_MANIFEST_FILE := $(LOCAL_PATH)/manifest.xml
